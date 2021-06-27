d34260a4c243f4096a60cf9b0dc5f696cfa7d43e
Chris Opthoog <chris.opthoog@myob.com>
2021-06-28 09:26:43 +1200

    updating default to be consistent

--- src/StringGenerator.Client/Options.cs
+++ src/StringGenerator.Client/Options.cs
@@ -5,7 +5,7 @@ namespace StringGenerator.Client {
         [Option('b', "batch", Required = false, Default = 1, HelpText = "The number of passwords to generate.")]
         public int BatchSize { get; set; }
 
-        [Option('l', "length", Required = false, Default = 64, HelpText = "The length of the passwords to generate.")]
+        [Option('l', "length", Required = false, Default = 32, HelpText = "The length of the passwords to generate.")]
         public int Length { get; set; }
 
         [Option('s', "symbols", HelpText = "Use symbols in password (or not). If this switch is present then symbols will be used.")]
--- src/StringGenerator/StringGeneratorBase.cs
+++ src/StringGenerator/StringGeneratorBase.cs
@@ -25,18 +25,18 @@ namespace StringGenerator {
         /// Generate a random string to specified length using lower case letters, upper case letters, numbers 0-9 and optionally symbols.
         /// </summary>
         /// <param name="length">Default is 32.</param>
-        /// <param name="useSymbols">Default is true.</param>
+        /// <param name="useSymbols">Default is False.</param>
         /// <returns></returns>
-        public abstract string Next(int length = 32, bool useSymbols = true);
+        public abstract string Next(int length = 32, bool useSymbols = false);
 
         /// <summary>
         /// Generate batches of strings.
         /// </summary>
         /// <param name="batchLength">Default is 1.</param>
         /// <param name="length">Default is 32.</param>
-        /// <param name="useSymbols">Default is true.</param>
+        /// <param name="useSymbols">Default is False.</param>
         /// <returns></returns>
-        public abstract IEnumerable<string> NextBatch(int batchLength = 1, int length = 32, bool useSymbols = true);
+        public abstract IEnumerable<string> NextBatch(int batchLength = 1, int length = 32, bool useSymbols = false);
 
         
     }
