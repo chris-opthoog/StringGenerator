09512ba3a328890e3537e7f22f8d4303947fbbc8
Chris Opthoog <chris.opthoog@myob.com>
2021-07-01 10:27:37 +1200

    trying to fix gitversion issue

--- build/Build.cs
+++ build/Build.cs
@@ -24,7 +24,7 @@ class Build : NukeBuild
 
     [GitRepository] readonly GitRepository GitRepository;
 
-    [GitVersion(Framework = "netcoreapp3.1", NoFetch = true)] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "net5.0", NoFetch = true)] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath OutputDirectory => RootDirectory / "output";
