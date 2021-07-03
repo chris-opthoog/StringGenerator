0fe895f6038a94a65cc61498f82d071d6b954b36
Chris Opthoog <chris.opthoog@myob.com>
2021-07-04 10:10:28 +1200

    updating example client

--- src/StringGenerator.Client/Program.cs
+++ src/StringGenerator.Client/Program.cs
@@ -6,11 +6,11 @@ namespace StringGenerator.Client {
     class Program {
         static void Main(string[] args) {
 
-
             Parser.Default.ParseArguments<Options>(args)
                    .WithParsed(opts => {
-                       using var g = new PseudoRandomStringGenerator();
-                       g.NextBatch(opts.BatchSize, opts.Length, opts.UseSymbols).ToList().ForEach(x => Console.WriteLine(x));
+                       CryptoStringGenerator.GetNextBatch(opts.BatchSize, opts.Length, opts.UseSymbols)
+                        .ToList()
+                        .ForEach(x => Console.WriteLine(x));
                    });
         }
     }
